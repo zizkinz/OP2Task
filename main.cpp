9492cb78ae973c4b61711715911c100cb567bc8c
maksimcizov <maksimcizov@gmail.com>
2021-09-09 15:19:33 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -5,35 +5,52 @@ using std::cin;
 using std::endl;
 using std::string;
 
-struct studentas
-{
+struct studentas {
     string vard, pavard;
-    float nd[10];
-    float egzam;
+    float nd[10]{};
+    float ekzam;
     float gal;
 };
 
-void pild(studentas &kint);
-void printas(studentas &kint);
 
-int main() {
-    int n;
+void pild(studentas &kint);
+void printas(studentas &kin);
+
+
+int main()
+{
+
     studentas st1;
-    studentas st2;
-    cout<<"New testdhsjjdsa";
-    cout << "Iveskite studento varda ir pavarde: ";
-    cin >> st1.vard >> st1.pavard;
-    cout << "Kiek namu darbu bus? (1-10)"; cin >> n;
-    cout << "Iveskite pazymius: "<<endl;
-    float sun = 0, vid = 0;
-    for (int i =0; i < n; i++){
-        cin>>st1.nd[i];
-        sun += st1.nd[i];
-    }
-    vid = sun / n;
-    cout << "Iveskite egzamino pazymi: "; cin >> st1.egzam;
-    st1.gal = vid*0.4 + 0.6 * st1.egzam;
-    cout << "Vidurkis: "<< vid;
-    cout << "Galutinis: " << st1.gal;
-    return 0;
+    studentas st2{ "Vardas2","Pavarde2",{5,8,7,4,5,8,9,5,4},9, 0};
+    //pild(st1);
+    //printas(st1);
+    studentas studentai[10];
+    for (int i = 0; i < 2; i++)
+        pild(studentai[i]);
+    for (int i = 0; i < 2; i++)
+        printas(studentai[i]);
+
 }
+
+void pild(studentas& kint) {
+    int n;
+    cout << "Iveskite studento varda ir pavarde: "; cin >> kint.vard >> kint.pavard;
+    cout << "Kiek namu darbu bus? (1-10)"; cin >> n;
+    cout << "Iveskite pazymius:" << endl;
+    float sum = 0, vid = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> kint.nd[i];
+        sum += kint.nd[i];
+    }
+    vid = sum / n;
+    cout << "Iveskite egzamino pazymi:"; cin >> kint.ekzam;
+    kint.gal = vid * 0.4 + kint.ekzam;
+}
+
+void printas(studentas& kin) {
+    cout << kin.vard << " " << kin.pavard<<" ";
+    for (int i = 0; i < sizeof(kin.nd) / sizeof(kin.nd[0]); i++)
+        cout << kin.nd[i] << " ";
+    cout << "Egzaminas = " << kin.ekzam << "; Galutinis: " << kin.gal<<endl;
+}
+
